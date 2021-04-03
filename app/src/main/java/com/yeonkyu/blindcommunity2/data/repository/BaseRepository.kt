de286f6367c5ba2e54ee5ab2b77a22940b36c79f
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-04-03 22:16:30 +0900

    Add BaseRepository

---
+++ app/src/main/java/com/yeonkyu/blindcommunity2/data/repository/BaseRepository.kt
@@ -0,0 +1,22 @@
+package com.yeonkyu.blindcommunity2.data.repository
+
+import org.json.JSONObject
+import retrofit2.Response
+
+abstract class BaseRepository {
+    suspend fun <Any> apiRequest(call: suspend () -> Response<Any>): Any{
+        val response = call.invoke()
+
+        if(response.isSuccessful){
+            return response.body()!!
+        }else{
+            val error = response.errorBody()?.toString()
+            val message = StringBuilder()
+
+            message.append(JSONObject(error!!).getString("message"))
+            message.append("ERROR Code : ${response.code()}")
+            throw Exception(message.toString())
+
+        }
+    }
+}
