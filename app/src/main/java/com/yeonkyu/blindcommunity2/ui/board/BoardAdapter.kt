803c93899f6fdff3b4a9ad169a75e0ab2e9b8c6d
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-04-14 03:57:47 +0900

    Fix BoardAdapter

--- app/src/main/java/com/yeonkyu/blindcommunity2/ui/board/BoardAdapter.kt
+++ app/src/main/java/com/yeonkyu/blindcommunity2/ui/board/BoardAdapter.kt
@@ -1,7 +1,6 @@
 package com.yeonkyu.blindcommunity2.ui.board
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +8,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.yeonkyu.blindcommunity2.R
 import com.yeonkyu.blindcommunity2.data.entities.BoardInfo
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class BoardAdapter(context: Context) : RecyclerView.Adapter<BoardAdapter.BoardViewHolder>() {
 
@@ -16,7 +18,7 @@ class BoardAdapter(context: Context) : RecyclerView.Adapter<BoardAdapter.BoardVi
     private val mBoardList = ArrayList<BoardInfo>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BoardViewHolder {
-        val view = LayoutInflater.from(mContext).inflate(R.layout.item_board_list,parent,false)
+        val view = LayoutInflater.from(mContext).inflate(R.layout.item_board_list, parent, false)
         return BoardViewHolder(view)
     }
 
@@ -39,7 +41,7 @@ class BoardAdapter(context: Context) : RecyclerView.Adapter<BoardAdapter.BoardVi
         notifyDataSetChanged()
     }
 
-    fun addLast(board:BoardInfo){
+    fun addLast(board: BoardInfo){
         mBoardList.add(board)
     }
 
@@ -53,8 +55,40 @@ class BoardAdapter(context: Context) : RecyclerView.Adapter<BoardAdapter.BoardVi
         fun onBind(board: BoardInfo){
             super.itemView
             titleText.text = board.title
-            timeText.text = board.postId
             nickname.text = board.nickname
+
+
+            val boardTime = board.postId?.split("_")!!
+
+            val dateNow = SimpleDateFormat("yyyyMMdd",Locale.KOREA)
+            val timeNow = SimpleDateFormat("HHmmss",Locale.KOREA)
+            val date = dateNow.format(Date(System.currentTimeMillis()))
+            val time = timeNow.format(Date(System.currentTimeMillis()))
+
+            if(date!=boardTime[0]){
+                val yearGap = date.substring(0,4).toInt() - boardTime[0].substring(0,4).toInt()
+                if(yearGap==0){
+                    val temp = boardTime[0].substring(4,6)+"/"+boardTime[0].substring(6,8)+" "+boardTime[1].substring(0,2)+":"+boardTime[1].substring(2,4)
+                    timeText.text = temp
+                }
+                else{
+                    val temp = boardTime[0].substring(2,4)+"/"+boardTime[0].substring(4,6)+"/"+boardTime[0].substring(6,8)+" "+boardTime[1].substring(0,2)+":"+boardTime[1].substring(2,4)
+                    timeText.text = temp
+                }
+            }
+            else{
+                val hourGap = time.substring(0,2).toInt() - boardTime[1].substring(0,2).toInt()
+                if(hourGap==0){
+                    val minGap = time.substring(2,4).toInt() - boardTime[1].substring(2,4).toInt()
+                    val temp = "${minGap}분 전"
+                    timeText.text = temp
+                }
+                else{
+                    val temp = "${hourGap}시간 전"
+                    timeText.text = temp
+                }
+            }
+
         }
     }
 }
