8a439ac694e922dac4f9f0f35e53f2a226955a53
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-04-21 22:32:28 +0900

    Add DataBinding module in BoardAdapter

--- app/src/main/java/com/yeonkyu/blindcommunity2/ui/board/BoardAdapter.kt
+++ app/src/main/java/com/yeonkyu/blindcommunity2/ui/board/BoardAdapter.kt
@@ -4,9 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.yeonkyu.blindcommunity2.R
 import com.yeonkyu.blindcommunity2.data.entities.BoardInfo
+import com.yeonkyu.blindcommunity2.databinding.ItemBoardListBinding
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -25,8 +27,8 @@ class BoardAdapter : RecyclerView.Adapter<BoardAdapter.BoardViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BoardViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_board_list, parent, false)
-        return BoardViewHolder(view)
+        val binding:ItemBoardListBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),R.layout.item_board_list,parent,false)
+        return BoardViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: BoardViewHolder, position: Int) {
@@ -55,50 +57,11 @@ class BoardAdapter : RecyclerView.Adapter<BoardAdapter.BoardViewHolder>() {
         boardList.add(board)
     }
 
-
-    inner class BoardViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
-        private val titleText = itemView.findViewById<TextView>(R.id.board_list_title)
-        private val timeText = itemView.findViewById<TextView>(R.id.board_list_time)
-        private val nickname = itemView.findViewById<TextView>(R.id.board_list_nickname)
+    inner class BoardViewHolder(private val binding: ItemBoardListBinding): RecyclerView.ViewHolder(binding.root){
 
         fun onBind(board: BoardInfo){
-            super.itemView
-            titleText.text = board.title
-            nickname.text = board.nickname
-
-
-            val boardTime = board.postId?.split("_")!!
-
-            val dateNow = SimpleDateFormat("yyyyMMdd",Locale.KOREA)
-            val timeNow = SimpleDateFormat("HHmmss",Locale.KOREA)
-            val date = dateNow.format(Date(System.currentTimeMillis()))
-            val time = timeNow.format(Date(System.currentTimeMillis()))
-
-            if(date!=boardTime[0]){
-                val yearGap = date.substring(0,4).toInt() - boardTime[0].substring(0,4).toInt()
-                if(yearGap==0){
-                    val temp = boardTime[0].substring(4,6)+"/"+boardTime[0].substring(6,8)+" "+boardTime[1].substring(0,2)+":"+boardTime[1].substring(2,4)
-                    timeText.text = temp
-                }
-                else{
-                    val temp = boardTime[0].substring(2,4)+"/"+boardTime[0].substring(4,6)+"/"+boardTime[0].substring(6,8)+" "+boardTime[1].substring(0,2)+":"+boardTime[1].substring(2,4)
-                    timeText.text = temp
-                }
-            }
-            else{
-                val hourGap = time.substring(0,2).toInt() - boardTime[1].substring(0,2).toInt()
-                if(hourGap==0){
-                    val minGap = time.substring(2,4).toInt() - boardTime[1].substring(2,4).toInt()
-                    val temp = "${minGap}분 전"
-                    timeText.text = temp
-                }
-                else{
-                    val temp = "${hourGap}시간 전"
-                    timeText.text = temp
-                }
-            }
-
+            binding.boardItem = board
+            binding.executePendingBindings()
         }
     }
 }
