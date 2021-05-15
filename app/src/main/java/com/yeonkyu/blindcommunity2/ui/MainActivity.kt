bc759de218a0910f01f50b2caabe633f79253b7c
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-05-16 01:03:25 +0900

    Revise fragment to ViewPager2 in MainActivity

--- app/src/main/java/com/yeonkyu/blindcommunity2/ui/MainActivity.kt
+++ app/src/main/java/com/yeonkyu/blindcommunity2/ui/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.yeonkyu.blindcommunity2.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.yeonkyu.blindcommunity2.R
+import com.yeonkyu.blindcommunity2.ui.account.AccountFragment
+import com.yeonkyu.blindcommunity2.ui.home.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,12 +23,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.main_bottom_navigation)
+        main_view_pager.isUserInputEnabled = false
+        main_view_pager.adapter = PagerAdapter(supportFragmentManager,lifecycle)
+        main_view_pager.registerOnPageChangeCallback(PageChangeCallback())
+        main_bottom_navigation.setOnNavigationItemSelectedListener { navigationSelected(it) }
+    }
 
-        NavigationUI.setupWithNavController(
-                bottomNavigationView,
-                findNavController(R.id.main_nav_host)
-        )
+    private fun navigationSelected(item: MenuItem): Boolean {
+        val checked = item.setChecked(true)
+        when (checked.itemId) {
+            R.id.home_screen -> {
+                main_view_pager.currentItem = 0
+                return true
+            }
+            R.id.account_screen -> {
+                main_view_pager.currentItem = 1
+                return true
+            }
+        }
+        return false
+    }
 
+    private inner class PagerAdapter(fm: FragmentManager, lc: Lifecycle): FragmentStateAdapter(fm, lc) {
+        override fun getItemCount() = 2
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> HomeFragment()
+                1 -> AccountFragment()
+                else -> error("no such position: $position")
+            }
+        }
+    }
+
+    private inner class PageChangeCallback: ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            main_bottom_navigation.selectedItemId = when (position) {
+                0 -> R.id.home_screen
+                1 -> R.id.account_screen
+                else -> error("no such position: $position")
+            }
+        }
     }
 }
